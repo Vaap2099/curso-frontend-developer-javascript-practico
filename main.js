efbb5d46043aa8256f3523f2e4da008a32b22d1a
Victor Alvarez <vaap2099@gmail.com>
2023-07-17 11:17:29 -0600

    menu de carrito y navegacion

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
+const shoppingCart = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuMobile.addEventListener ('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener ('click', toggleShoppingCart);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart () {
+    shoppingCart.classList.toggle('inactive');
 }
