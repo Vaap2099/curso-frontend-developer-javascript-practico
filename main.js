3ebcfe84ad37cc66d45efed4e6993a6f1efe9535
Victor Alvarez <vaap2099@gmail.com>
2023-07-17 12:30:39 -0600

    Productos

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
-const shoppingCart = document.querySelector('.product-detail')
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuMobile.addEventListener ('click', toggleMobileMenu);
@@ -38,6 +39,69 @@ function toggleShoppingCart () {
         desktopMenu.classList.add('inactive');
     }
     shoppingCart.classList.toggle('inactive');
-    
-    
 }
+ const productList = [];
+ productList.push({
+    name: "Bike",
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+ }
+ );
+ productList.push({
+    name: "Monitor",
+    price: 80,
+    image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+ }
+ );
+ productList.push({
+    name: "Keyboard",
+    price: 20,
+    image: 'https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+ }
+ );
+ productList.push({
+    name: "Helmet",
+    price: 25,
+    image: 'https://media.istockphoto.com/id/157475434/es/foto/casco-de-ciclista-en-bicicleta-amarillo-blanco.jpg?s=2048x2048&w=is&k=20&c=xjixcd4ssA4_i25rWNHUkL0NflxkDd0TyNKjjplz5MA=',
+ }
+ );
+
+ function renderProducts(arr) {
+    
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('figure');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+    }
+ }
+
+ renderProducts(productList);
